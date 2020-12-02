d4f7d6e86d20bba7a61c4729ab93978a76c9f450
Richard Vong <richard.vong192@gmail.com>
2020-12-03 11:48:55 +1300

    Add files via upload

---
+++ lib/image.ex
@@ -0,0 +1,6 @@
+defmodule Identicon.Image do
+
+    #Image struct sole purpose is to hold all the differnt types of data in our application
+    defstruct hex: nil, color: nil, grid: nil, pixel_map: nil
+
+end
